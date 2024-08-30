dd6d8e8587664d5b456386f3c0ff44bdcf5b9e38
Jae <gohyishen777@gmail.com>
2024-08-30 16:07:07 +0800

    feature: created login page

--- cgh_project/src/App.js
+++ cgh_project/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import ScrollToTop from "./components/ScrollToTop";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages_login/LoginPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ScrollToTop/>
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ cgh_project/src/pages_login/LoginPage.js
@@ -0,0 +1,18 @@
+import "../styles/loginpage.css";
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className="login-page">
+        <div className="left-col">
+          <h1>This is left column</h1>
+        </div>
+        <div className="right-col">
+          <h1>This is right column</h1>{" "}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;

a733b681a2081c9bac23bef3f2abb21c42bd877f
Jae <gohyishen777@gmail.com>
2024-09-06 17:15:40 +0800

    feature\ added calendar in staff home page

--- cgh_project/src/pages_staff/StaffHomePage.js
+++ cgh_project/src/pages_staff/StaffHomePage.js
@@ -1,14 +1,25 @@
 import "../styles/staffhomepage.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/footer";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 const StaffHomePage = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
     <>
       <Navbar homeRoute={"/staff-home"} />
       <div className="staff-home-page">
         <div className="staff-home-div-left">
-          <div className="staff-home-div-left-base"></div>
+          <div className="staff-home-div-left-base">
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+              inline
+            />
+          </div>
         </div>
         <div className="staff-home-div-mid">
           <div className="staff-home-div-mid-base"></div>
